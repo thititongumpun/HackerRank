095301aa4ef5017df14b33249962281006bd9265
HOMEPRO\thitito <thitito@homepro.co.th>
2021-03-09 11:01:58 +0700

    checkin 3 problem

--- HackkerRank/Program.cs
+++ HackkerRank/Program.cs
@@ -10,6 +10,52 @@ namespace HackkerRank
     {
         static void Main(string[] args)
         {
+            int result = solveMeFirst(5, 6);
+            Console.WriteLine(result);
+
+            int[] arr = { 1, 2, 3, 4, 10, 11 };
+            int result2 = simpleArraySum(arr);
+            Console.WriteLine(result2);
+
+            long[] arr2 = { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+            long result3 = aVeryBigSum(arr2);
+            Console.WriteLine(result3);
+        }
+
+        /// <summary>
+        /// solveMeFirst
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static int solveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// simpleArraySum
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        static int simpleArraySum(int[] arr)
+        {
+            int total = 0;
+            for(int i=0;i<arr.Length;++i)
+            {
+                total += arr[i];
+            }
+            return total;
+        }
+
+        static long aVeryBigSum(long[] arr)
+        {
+            long total = 0;
+            for(int i=0;i<arr.Length; ++i)
+            {
+                total += arr[i];
+            }
+            return total;
         }
     }
 }
